d7e55509ab4d39e2e72e44c960054d2a43becaa2
The_Ring_coder <ryuproductionstudios5015@gmail.com>
2022-06-04 05:05:23 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #!/bin/bash
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Return: 0 if Success
@@ -7,7 +8,6 @@
  */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
+	puts("\"Programming is like building a multilingual puzzel");
+	return (0);
 }
