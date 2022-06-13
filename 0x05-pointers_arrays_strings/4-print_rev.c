7744ddcaa83a5f65209454cc156a13772159de36
The_Ring_coder <ryuproductionstudios5015@gmail.com>
2022-06-13 04:20:22 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _rev prints a string in reverse and a new line
+ * 
+ * Return: 0
+ */
+
+void print_rev(char *s);
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while (i--)
+		_putchar(s[i];
+	_putchar('\n');
+}
