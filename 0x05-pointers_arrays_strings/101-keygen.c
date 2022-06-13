91d27a8101d4d4b05ec28698922c30c222a6c8c7
The_Ring_coder <ryuproductionstudios5015@gmail.com>
2022-06-13 07:34:08 -0700

    Task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,10 +1,10 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 /**
  * main - generates random passwords for 101-crackme
- * Return: zero
+ * Return: 0
  */
 int main(void)
 {
