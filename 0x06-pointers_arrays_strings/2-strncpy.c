9206db61159c7acf48da25cede8af37cc3b4d94f
The_Ring_coder <ryuproductionstudios5015@gmail.com>
2022-06-16 09:39:07 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,24 +9,16 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int srclen = 0, i = 0;
-	char *temp = dest, *start = src;
+	int index = 0, src_len = 0;
+	
+	while (src[index++])
+		src_len++;
 
-	while (*src)
-	{
-		srclen++;
-		src++;
-	}
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
 
-	srclen++;
-
-	if (n > srclen)
-		n = srclen;
-
-	src = start;
-
-	for (; i < n; i++)
-		*dest++ = *src++;
-
-	return (temp);
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+	return (dest);
 }
+	
