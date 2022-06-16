5d42c79dba8328bc5812a7153a661aac1efff532
The_Ring_coder <ryuproductionstudios5015@gmail.com>
2022-06-16 07:30:43 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,7 +4,6 @@
  * *_strncat - Concatenates @src to @dest
  * @src: the source string to append to @dest
  * @dest: the destiation string to be concatenated upon
- *
  * Return: pointer to the resulting string
  */
 
@@ -15,9 +14,7 @@ char *_strcat(char *dest, char *src)
 
 	while (dest[index++])
 		dest_len++;
-
 	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
-
-	return (dest);
+	return (index);
 }
